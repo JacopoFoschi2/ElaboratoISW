2d41f92387c08b60d4a7f5a0c6c76246c2118f48
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-01 13:57:41 +0100

    added games_controller and started prototipying some functions to be implemented

---
+++ backend/src/controllers/games_controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import { connection } from "../utils/db_connection"
+
+export async function allGamesOfGenre(req: Request, res: Response) {
+    connection.execute(
+        ``,
+        [req.params.id],
+        function (err, results, fields) {
+            res.json(results)
+        }
+    )
+};
+
+export async function gameFromID(req: Request, res: Response) {
+    connection.execute(
+        ``,
+        [req.params.id],
+        function (err, results, fields) {
+            res.json(results)
+        }
+    )
+};
+
