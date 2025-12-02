a6f8c414142e60d7948ed176cfc2381a583a0f29
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-02 10:12:05 +0100

    feat: added users_router to be implemented

---
+++ backend/src/routes/users_router.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as forumController from '../controllers/forum_controller';
+
+const reviewsRouter = Router();
+
+// TODO: Implement reviews routes
+
+export default reviewsRouter;
