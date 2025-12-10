69a72984c2e71db6c9bdfc4d7562ddd7db8cf867
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2025-12-10 23:33:24 +0100

    chore: added the initial style of the homepage

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import { createRouter, createWebHistory, Router } from "vue-router"
-import "./style.css"
+// import "./style.css"
 import App from "./App.vue"
 import Home from "./pages/Home.vue"
 import NotFound from "./pages/NotFound.vue"
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="homepage">
+
+        <h1>THE HOME PAGE</h1>
+
+        <main class="content-wrapper">
+            <h2 class="section-title">Action</h2>
+            <div class="game-section">
+                <div class="game-card">
+                    <!-- Here the list of the games -->
+                </div>
+            </div>
+        </main>
+    </div>
+    
+</template>
+<script>
+
+
+</script>
+
+<style scoped>
+    .homepage {
+        display: flex;
+        flex-direction: column;
+        background-color: #270c37;
+    }
+    
+</style>
