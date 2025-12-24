71230f1fe794898a97d67c5b30c60d847230ae92
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-24 19:06:49 +0100

    feat: implemented user router. Login to be implemented later

--- backend/src/routes/users-router.ts
+++ backend/src/routes/users-router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import * as usersController from '../controllers/users-controller';
 
-const reviewsRouter = Router();
+const router: Router = Router();
 
-// TODO: Implement reviews routes
+router.get('/api/users', usersController.allUsersAndPrivileges);
+router.post('/api/user/modify/:id', usersController.modifyUser);
+router.post('/api/user/delete/:id', usersController.removeUser);
+router.post('/api/user/add', usersController.addNewUser);
 
-export default reviewsRouter;
+export default router;
