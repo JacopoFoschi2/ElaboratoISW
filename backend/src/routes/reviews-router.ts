60dc72b60e4bb3e16464b728c8f782124f733ed2
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-24 19:15:25 +0100

    fix: removed TODO comment in reviews router

--- backend/src/routes/reviews-router.ts
+++ backend/src/routes/reviews-router.ts
@@ -3,7 +3,6 @@ import * as reviewsController from '../controllers/reviews-controller';
 
 const router = Router();
 
-//TODO: Implement review routes
 router.get('/api/reviews/game/:id', reviewsController.allReviewsOfGame);
 router.get('/api/reviews/user/:id', reviewsController.reviewOfUser);
 router.post('/api/reviews/game/:gameName/user/:userId', reviewsController.addReview);
