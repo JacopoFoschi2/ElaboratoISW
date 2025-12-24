746c6f794b078fb7f843f46520650b579e23e455
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-24 19:16:26 +0100

    fix: replaced gameId with gameName in routes to be more coherent with DB

--- backend/src/routes/forum-router.ts
+++ backend/src/routes/forum-router.ts
@@ -4,9 +4,9 @@ import * as forumController from "../controllers/forum-controller";
 const router: Router = Router();
 
 router.get('/api/forums', forumController.allForums);
-router.get('/api/forums/game/:gameId/comments', forumController.allCommentsOfGame);
-router.post('/api/forums/game/:gameId/comment', forumController.addComment);
-router.put('/api/forums/game/:gameId/comment/:commentId', forumController.modifyComment);
-router.delete('/api/forums/game/:gameId/comment/:commentId', forumController.removeComment);
+router.get('/api/forums/game/:gameName/comments', forumController.allCommentsOfGame);
+router.post('/api/forums/game/:gameName/comment', forumController.addComment);
+router.put('/api/forums/game/:gameName/comment/:commentId', forumController.modifyComment);
+router.delete('/api/forums/game/:gameName/comment/:commentId', forumController.removeComment);
 
 export default router;
