cf2dd1bac3427fcaec64f7491d586bd90610ca2c
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-24 19:12:13 +0100

    feat: implemented forum router

--- backend/src/routes/forum-router.ts
+++ backend/src/routes/forum-router.ts
@@ -3,6 +3,10 @@ import * as forumController from "../controllers/forum-controller";
 
 const router: Router = Router();
 
-//TODO
+router.get('/api/forums', forumController.allForums);
+router.get('/api/forums/game/:gameId/comments', forumController.allCommentsOfGame);
+router.post('/api/forums/game/:gameId/comment', forumController.addComment);
+router.put('/api/forums/game/:gameId/comment/:commentId', forumController.modifyComment);
+router.delete('/api/forums/game/:gameId/comment/:commentId', forumController.removeComment);
 
 export default router;
