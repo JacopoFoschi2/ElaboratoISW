159fac03cb2a41673cd06839d7140168fe28b5cf
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-24 19:14:54 +0100

    feat: implemented reviews router

--- backend/src/routes/reviews-router.ts
+++ backend/src/routes/reviews-router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import * as reviewsController from '../controllers/reviews-controller';
 
-const reviewsRouter = Router();
+const router = Router();
 
 //TODO: Implement review routes
+router.get('/api/reviews/game/:id', reviewsController.allReviewsOfGame);
+router.get('/api/reviews/user/:id', reviewsController.reviewOfUser);
+router.post('/api/reviews/game/:gameName/user/:userId', reviewsController.addReview);
+router.put('/api/reviews/game/:gameName/user/:userId', reviewsController.modifyReview);
+router.delete('/api/reviews/game/:gameName/user/:userId', reviewsController.removeReview);
 
-export default reviewsRouter;
+export default router;
