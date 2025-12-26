09433bf34ad24274a4038672ba36c686d50786bf
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2025-12-26 16:34:20 +0100

    fix: made scss actually work, to make it compile in the frontend it needs the command: npm run sass

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue"
 import { createRouter, createWebHistory, Router } from "vue-router"
-import "./style.scss"
 import App from "./App.vue"
 import Home from "./pages/Home.vue"
 import NotFound from "./pages/NotFound.vue"
