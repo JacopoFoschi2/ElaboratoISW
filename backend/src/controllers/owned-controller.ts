eca58134b535230531d68472ac5c808903f06c10
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-29 20:03:52 +0100

    feat: implemented owned controller

--- backend/src/controllers/owned-controller.ts
+++ backend/src/controllers/owned-controller.ts
@@ -1,42 +1,35 @@
 import type { Request, Response } from "express"
 import { connection } from "../utils/db-connection"
+import { handleExists, handleQueryOutput } from "../utils/query-handling"
 
 export async function addToOwned(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `INSERT INTO owned (userId, gameId) VALUES (?, ?)`,
+        [req.params["userId"], req.params["gameId"]],
+        handleQueryOutput(201, res)
     )
 };
 
 export async function listOwnedOfUser(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT gameId FROM owned WHERE userId = ?`,
+        [req.params["userId"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function deleteFromOwned(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `DELETE FROM owned WHERE userId = ? AND gameId = ?`,
+        [req.params["userId"], req.params["gameId"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function isOwned(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT * FROM owned WHERE userId = ? AND gameId = ?`,
+        [req.params["userId"], req.params["gameId"]],
+        handleExists(res)
     )
 };
