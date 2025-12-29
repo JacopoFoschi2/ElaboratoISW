f9996f758738499e5142a7d516daeed7b51c2922
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-29 19:17:51 +0100

    fix: fixed incorrect field in function return that made the connection.execute break

--- backend/src/utils/query-handling.ts
+++ backend/src/utils/query-handling.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 
 export function handleQueryOutput(code: number, res: Response){
-    return function (err: any, results: any, res: Response) {
+    return function (err: any, results: any, fields: any) {
     if (err) {
         res.status(500).json({ error: err.message });
         return;
