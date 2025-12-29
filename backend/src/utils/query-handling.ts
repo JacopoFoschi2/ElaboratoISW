e0f80197d2ff095e7b828218de29a294fe88e18a
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-29 20:02:51 +0100

    feat: added exists handling to query handling utils

--- backend/src/utils/query-handling.ts
+++ backend/src/utils/query-handling.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 
-export function handleQueryOutput(code: number, res: Response){
+export function handleQueryOutput(code: number, res: Response) {
     return function (err: any, results: any, fields: any) {
     if (err) {
         res.status(500).json({ error: err.message });
@@ -8,4 +8,14 @@ export function handleQueryOutput(code: number, res: Response){
     }
     res.status(code).json(results);
     };
+}
+
+export function handleExists(res: Response) {
+  return function (err: any, results: any, fields: any) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.status(200).json({ "exists": results.length > 0 });
+  };
 }
