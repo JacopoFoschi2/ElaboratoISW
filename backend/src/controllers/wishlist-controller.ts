52a892657985b5f10ff50d29970303543e63b2ef
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-29 20:08:40 +0100

    feat: implemented wishlist controller

--- backend/src/controllers/wishlist-controller.ts
+++ backend/src/controllers/wishlist-controller.ts
@@ -1,42 +1,35 @@
 import type { Request, Response } from "express"
 import { connection } from "../utils/db-connection"
+import { handleExists, handleQueryOutput } from "../utils/query-handling"
 
 export async function addToWishlist(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `INSERT INTO wishlist (userId, gameId) VALUES (?, ?)`,
+        [req.params["userId"], req.params["gameId"]],
+        handleQueryOutput(201, res)
     )
 };
 
 export async function listWishlistOfUser(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT gameId FROM wishlist WHERE userId = ?`,
+        [req.params["userId"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function deleteFromWishlist(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `DELETE FROM wishlist WHERE userId = ? AND gameId = ?`,
+        [req.params["userId"], req.params["gameId"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function isGameInWishlist(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT * FROM wishlist WHERE userId = ? AND gameId = ?`,
+        [req.params["userId"], req.params["gameId"]],
+        handleExists(res)
     )
 };
