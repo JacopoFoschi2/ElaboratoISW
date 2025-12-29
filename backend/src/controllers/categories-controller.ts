4e3dcab3aed7694b9e2c1dbae06e0b102d86f178
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-29 19:21:41 +0100

    feat: implemented all queries of categories controller

--- backend/src/controllers/categories-controller.ts
+++ backend/src/controllers/categories-controller.ts
@@ -1,42 +1,35 @@
 import type { Request, Response } from "express"
 import { connection } from "../utils/db-connection"
+import { handleQueryOutput } from "../utils/query-handling";
 
 export async function createCategory(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
-    )
+        `INSERT INTO categories (categoryName) VALUES (?)`,
+        [req.body["categoryName"]],
+        handleQueryOutput(201, res)
+    );
 };
 
 export async function listCategories(req: Request, res: Response) {
     connection.execute(
-        ``,
+        `SELECT * FROM categories`,
         [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        handleQueryOutput(200, res)
     )
 };
 
 export async function updateCategory(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `UPDATE categories SET categoryName = ? WHERE categoryId = ?`,
+        [req.body["categoryName"], req.params["id"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function deleteCategory(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `DELETE FROM categories WHERE categoryId = ?`,
+        [req.params["id"]],
+        handleQueryOutput(200, res)
     )
 };
