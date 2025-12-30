6c443c2f1588aece4fdba551d66ab1c00b695769
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-30 18:57:24 +0100

    feat: should have FINALLY implemented a games seed that works

--- backend/db/seed/seed.ts
+++ backend/db/seed/seed.ts
@@ -2,18 +2,18 @@ import fs from "fs";
 import path from "path";
 import { connection } from "../../src/utils/db-connection";
 
+type Game = {
+  imagesName: string;
+  gameName: string;
+  gameDesc: string;
+  gameReleaseDate: string;
+  gameSteamLink: string | null;
+  gameGoGLink: string | null;
+  gameEpicLink: string | null;
+};
+
 function readImage(basePath: string, folder: string, fileName: string) {
   return fs.readFileSync(path.join(basePath, folder, fileName));
-}
-
-type Game = {
-    imagesName: string;
-    gameName: string;
-    gameDesc: string;
-    gameReleaseDate: string;
-    gameSteamLink: string | null;
-    gameGoGLink: string | null;
-    gameEpicLink: string | null;
 }
 
 const jsonPath = path.resolve(__dirname, "games.json");
@@ -23,3 +23,60 @@ const games: Game[] = JSON.parse(content);
 const covers = "covers";
 const bigBanners = "bigBanners";
 const smallBanners = "smallBanners";
+const assetsPath = path.resolve(__dirname, "assets");
+
+const values: string[] = [];
+const params: any[] = [];
+const baseQuery = `
+    INSERT INTO games (
+        gameName,
+        gameDesc,
+        gameSteamLink,
+        gameGoGLink,
+        gameEpicLink,
+        gameReleaseDate,
+        gameSmallBannerName,
+        gameSmallBannerBin,
+        gameCoverName,
+        gameCoverBin,
+        gameBigBannerName,
+        gameBigBannerBin
+    )
+    VALUES 
+    `;
+
+for (const game of games) {
+  const coverBin = readImage(assetsPath, covers, game.imagesName);
+  const smallBannerBin = readImage(assetsPath, smallBanners, game.imagesName);
+  const bigBannerBin = readImage(assetsPath, bigBanners, game.imagesName);
+  values.push("(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  params.push(
+    game.gameName,
+    game.gameDesc,
+    game.gameSteamLink,
+    game.gameGoGLink,
+    game.gameEpicLink,
+    game.gameReleaseDate,
+    game.imagesName,
+    smallBannerBin,
+    game.imagesName,
+    coverBin,
+    game.imagesName,
+    bigBannerBin
+  );
+}
+
+const finalQuery = baseQuery + values.join(",\n");
+connection.execute(
+  finalQuery,
+  params,
+  function (err: any, results: any, fields: any) {
+    if (err) {
+      console.error("Error seeding database:", err);
+      return;
+    } else {
+      console.log("Database seeded successfully.");
+    }
+    connection.end();
+  }
+);
