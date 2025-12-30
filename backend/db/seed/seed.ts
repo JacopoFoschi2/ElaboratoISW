26243ef761c0346d068a0a24bb055bed79b86a96
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-30 16:59:41 +0100

    feat: added type Game to adhere to typescript

--- backend/db/seed/seed.ts
+++ backend/db/seed/seed.ts
@@ -5,3 +5,13 @@ import { connection } from "../../src/utils/db-connection";
 function readImage(basePath: string, folder: string, fileName: string) {
   return fs.readFileSync(path.join(basePath, folder, fileName));
 }
+
+type Game = {
+    imagesName: string;
+    gameName: string;
+    gameDesc: string;
+    gameReleaseDate: string;
+    gameSteamLink: string | null;
+    gameGoGLink: string | null;
+    gameEpicLink: string | null;
+}
