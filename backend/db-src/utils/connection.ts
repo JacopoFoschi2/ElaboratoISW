b64be714a7897e60b2856a80037331d6373dd178
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-12-31 12:31:58 +0100

    feat: added dbless connection that permits me to create db

--- backend/db-src/utils/connection.ts
+++ backend/db-src/utils/connection.ts
@@ -1,5 +1,12 @@
 import mysql, { Connection } from "mysql2";
 
+export const CreateDbConnection: Connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  multipleStatements: true
+})
+
 export const connection: Connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
