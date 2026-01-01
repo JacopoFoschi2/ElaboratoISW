fb30bdf3124edcd91a893141411649c94ace4476
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-01 14:51:08 +0100

    refactor: rewrote file to use new utils

--- backend/db-src/seeding-steps/games.ts
+++ backend/db-src/seeding-steps/games.ts
@@ -1,78 +1,65 @@
-import fs from "fs";
-import path from "path";
 import { Game } from "../types/game";
-import { readImage } from "../utils/seeding-utils";
+import {
+  getBaseAssetsPath,
+  insertRecord,
+  readImage,
+  readJson,
+  seedData,
+} from "../utils/seeding-utils";
 import { connection } from "../utils/connection";
 
-function insertGame(game: Game): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const coverBin = readImage(assetsPath, covers, game.imagesName);
-    const smallBannerBin = readImage(assetsPath, smallBanners, game.imagesName);
-    const bigBannerBin = readImage(assetsPath, bigBanners, game.imagesName);
-    const params = [
-      game.gameName,
-      game.gameDesc,
-      game.gameSteamLink,
-      game.gameGoGLink,
-      game.gameEpicLink,
-      game.gameReleaseDate,
-      game.imagesName,
-      smallBannerBin,
-      game.imagesName,
-      coverBin,
-      game.imagesName,
-      bigBannerBin,
-    ];
-    connection.execute(
-      query,
-      params,
-      function (err: any, results: any, fields: any) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      }
-    );
-  });
-}
-
-const jsonPath = path.resolve(process.cwd(), "db-src/data/games.json");
-const content = fs.readFileSync(jsonPath, "utf-8");
-const games: Game[] = JSON.parse(content);
+const games: Game[] = readJson("db-src/data/games.json");
 
 const covers = "covers";
 const bigBanners = "bigBanners";
 const smallBanners = "smallBanners";
-const assetsPath = path.resolve(process.cwd(), "db-src/assets");
+const assetsPath = getBaseAssetsPath("db-src/assets");
 
 const query = `
-    INSERT INTO games (
-        gameName,
-        gameDesc,
-        gameSteamLink,
-        gameGoGLink,
-        gameEpicLink,
-        gameReleaseDate,
-        gameSmallBannerName,
-        gameSmallBannerBin,
-        gameCoverName,
-        gameCoverBin,
-        gameBigBannerName,
-        gameBigBannerBin
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+  INSERT INTO games (
+      gameName,
+      gameDesc,
+      gameSteamLink,
+      gameGoGLink,
+      gameEpicLink,
+      gameReleaseDate,
+      gameSmallBannerName,
+      gameSmallBannerBin,
+      gameCoverName,
+      gameCoverBin,
+      gameBigBannerName,
+      gameBigBannerBin
+  )
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+function insertGame(game: Game): Promise<void> {
+  const coverBin = readImage(assetsPath, covers, game.imagesName);
+  const smallBannerBin = readImage(assetsPath, smallBanners, game.imagesName);
+  const bigBannerBin = readImage(assetsPath, bigBanners, game.imagesName);
+  const params = [
+    game.gameName,
+    game.gameDesc,
+    game.gameSteamLink,
+    game.gameGoGLink,
+    game.gameEpicLink,
+    game.gameReleaseDate,
+    game.imagesName,
+    smallBannerBin,
+    game.imagesName,
+    coverBin,
+    game.imagesName,
+    bigBannerBin,
+  ];
+  return insertRecord(connection, params, query);
+}
 
 export async function seedGames() {
-  try {
-    for (const game of games) {
-      await insertGame(game);
-    }
-  } catch (error) {
-    console.error("Error seeding games:", error);
-  } finally {
-    console.log("Seeding completed.");
-    connection.end();
-  }
+  await seedData(
+    games,
+    insertGame,
+    connection,
+    "Error seeding games:",
+    "Games seeding completed."
+  );
 }
