776d7dbb2218a6a897d7cafce083fd39cf620c60
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-02 11:20:25 +0100

    feat: implemented forum-controllor, auth still needed

--- backend/src/controllers/forum-controller.ts
+++ backend/src/controllers/forum-controller.ts
@@ -1,52 +1,43 @@
 import type { Request, Response } from "express"
 import { connection } from "../utils/db-connection"
+import { handleQueryOutput } from "../utils/query-handling"
 
 export async function addComment(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `INSERT INTO comments (gameId, userId, commentBody, commentTimeStamp) VALUES (?, ?, ?, NOW())`,
+        [req.body.gameId, req.body.userId, req.body.commentBody],
+        handleQueryOutput(201, res)
     )
 };
 
 export async function listForums(req: Request, res: Response) {
     connection.execute(
-        ``,
+        `SELECT gameName, gameSmallBannerName, gameSmallBannerBin FROM games`,
         [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        handleQueryOutput(200, res)
     )
 };
 
 export async function listCommentsOfGame(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params['id']],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT commentBody, commentTimeStamp, commentWasEdited, userUsername, userIconBin, userIconName FROM comments as c join users as u ON c.userId = u.userId WHERE gameId = ?`,
+        [req.params['gameId']],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function updateComment(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `INSERT INTO comments (commentBody, commentWasEdited) VALUES (?, true) WHERE commentId = ?`,
+        [req.body.commentBody, req.params['commentId']],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function deleteComment(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `DELETE FROM comments WHERE commentId = ?`,
+        [req.params['commentId']],
+        handleQueryOutput(200, res)
     )
 };
