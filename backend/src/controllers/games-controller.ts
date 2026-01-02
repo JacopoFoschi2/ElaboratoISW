c121e08ac009e51e8e9128f4eb00c394f53e6439
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-02 11:37:53 +0100

    feat: implemented games controller

--- backend/src/controllers/games-controller.ts
+++ backend/src/controllers/games-controller.ts
@@ -1,62 +1,55 @@
 import type { Request, Response } from "express"
 import { connection } from "../utils/db-connection"
+import { handleQueryOutput } from "../utils/query-handling"
 
 export async function createGame(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `INSERT INTO games (gameName, gameDesc, gameSteamLink, gameEpicLink, gameGoGLink, gameReleaseDate, gameSmallBannerName, gameSmallBannerBin, gameCoverName, gameCoverBin, gameBigBannerName, gameBigBannerBin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [req.body["gameName"], req.body["gameDesc"], req.body["gameSteamLink"], req.body["gameEpicLink"], req.body["gameGoGLink"], req.body["gameReleaseDate"], req.body["gameSmallBannerName"], req.body["gameSmallBannerBin"], req.body["gameCoverName"], req.body["gameCoverBin"], req.body["gameBigBannerName"], req.body["gameBigBannerBin"]],
+        handleQueryOutput(201, res)
     )
 };
 
 export async function listGamesOfGenre(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `SELECT gameId, gameName, gameCoverBin, gameCoverName FROM games as g
+         JOIN game_categories as c ON g.gameId = c.gameId
+         WHERE c.categoryId = ?`,
+        [req.params["genreId"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function listGamesOrderedByRating(req: Request, res: Response) {
     connection.execute(
-        ``,
+        `SELECT gameId, gameName, gameCoverBin, gameCoverName FROM games
+         ORDER BY (SELECT AVG(reviewRating) FROM reviews WHERE reviews.gameId = games.gameId) DESC`,
         [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        handleQueryOutput(200, res)
     )
 };
 
 export async function listGamesOrderedByRelease(req: Request, res: Response) {
     connection.execute(
-        ``,
+        `SELECT gameId, gameName, gameCoverBin, gameCoverName FROM games
+         ORDER BY gameReleaseDate DESC`,
         [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        handleQueryOutput(200, res)
     )
 };
 
 export async function updateGame(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `UPDATE games SET gameName = ?, gameDesc = ?, gameSteamLink = ?, gameEpicLink = ?, gameGoGLink = ?, gameReleaseDate = ?, gameSmallBannerName = ?, gameSmallBannerBin = ?, gameCoverName = ?, gameCoverBin = ?, gameBigBannerName = ?, gameBigBannerBin = ? WHERE gameId = ?`,
+        [req.body["gameName"], req.body["gameDesc"], req.body["gameSteamLink"], req.body["gameEpicLink"], req.body["gameGoGLink"], req.body["gameReleaseDate"], req.body["gameSmallBannerName"], req.body["gameSmallBannerBin"], req.body["gameCoverName"], req.body["gameCoverBin"], req.body["gameBigBannerName"], req.body["gameBigBannerBin"], req.params["id"]],
+        handleQueryOutput(200, res)
     )
 };
 
 export async function deleteGame(req: Request, res: Response) {
     connection.execute(
-        ``,
-        [req.params["id"]],
-        function (err, results, fields) {
-            res.json(results)
-        }
+        `DELETE FROM games WHERE gameId = ?`,
+        [req.params["gameId"]],
+        handleQueryOutput(200, res)
     )
 };
