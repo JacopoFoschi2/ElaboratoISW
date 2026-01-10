e61989c2f254d27f287a33bc7c61ea950f988254
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-10 15:35:24 +0100

    chore: did the skeleton of the game detail component

--- frontend/src/components/GameDetail.vue
+++ frontend/src/components/GameDetail.vue
@@ -2,11 +2,45 @@
 
 </script>
 <template>
+    <div class="game-detail-page">
+        <main class="content-wrapper">
+            <img src="#" alt="game cover" />
+            <h2 class="game-title">Game Title</h2>
 
+            <p class="game-description">Description of the game</p>
+
+
+        </main>
+
+    </div>
 </template>
 
 <style scoped lang="scss">
-    @use "../styles/style-variables.scss" as style-variables;
-    
+@use "../styles/style-variables.scss" as style-variables;
 
+.game-detail-page {
+    margin: 0;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: style-variables.$default-background-color;
+    overflow-y: auto;
+}
+
+.game-title {
+    font-size: 3rem;
+    color: style-variables.$primary-text-color;
+}
+
+.game-description {
+    font-size: 1.5rem;
+    color: style-variables.$secondary-text-color;
+}
+
+.content-wrapper {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 20px;
+    padding-bottom: 3%;
+}
 </style>
