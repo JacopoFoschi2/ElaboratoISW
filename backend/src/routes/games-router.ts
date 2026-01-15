1c03856bef6c4e5a66057f73b4cdd368580fd9bc
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-15 17:06:59 +0100

    feat: added routes to the two previous controllers

--- backend/src/routes/games-router.ts
+++ backend/src/routes/games-router.ts
@@ -7,6 +7,8 @@ router.post('/api/games', gamesController.createGame);
 router.get('/api/games/rating', gamesController.listGamesOrderedByRating);
 router.get('/api/games/release', gamesController.listGamesOrderedByRelease);
 router.get('/api/games/:genreId', gamesController.listGamesOfGenre);
+router.get('/api/games/as-you-type/:partialName', gamesController.listGamesAsYouType);
+router.get('/api/games/matching/:partialName', gamesController.listGamesMatching);
 router.get('/api/game/:gameId', gamesController.getGame);
 router.put('/api/games/:gameId', gamesController.updateGame);
 router.delete('/api/games/:gameId', gamesController.deleteGame);
