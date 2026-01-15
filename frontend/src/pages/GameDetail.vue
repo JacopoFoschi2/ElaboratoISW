6942d833b03321f9c120e5a9ae9a98fd0577b7cb
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-15 16:25:18 +0100

    fix: moved the GameDetail.vue to be a page and started to give it the mysql images

---
+++ frontend/src/pages/GameDetail.vue
@@ -0,0 +1,99 @@
+<script>
+import { ref, onMounted } from 'vue';
+import { routeLocationKey, useRoute } from 'vue-router';
+import StarRating from '../components/StarRating.vue';
+
+const route = useRoute();
+const game = ref(null);
+const loading = ref(true);
+
+const getImageUrl = (imageBuffer) => {
+    if (!imageBuffer || !imageBuffer.data) return '';
+
+    const binary = String.fromCharCode(...imageBuffer.data);
+    const base64String = window.btoa(binary);
+    return `data:image/png;base64,${base64String}`;
+};
+
+onMounted(async () => {
+    try {
+        const gameId = route.params.id;
+        const response = await fetch(`http://localhost:3000/api/game/${gameId}`);
+        const data = await response.json();
+        game.value = data[0];
+    } catch (error) {
+        console.error('There was a problem with the loading of the game:', error);
+    } finally {
+        loading.value = false;
+    }
+});
+
+</script>
+<template>
+    <div v-if="!loading && game" class="game-detail-page">
+        <main class="content-wrapper">
+            <img :src="getImageUrl(game.gameCoverBin)" :alt="game.gameName" class="game-cover" />
+            <h2 class="game-title">{{ game.gameName }}</h2>
+
+            <StarRating :rating="game.rating" :size="30" /> <!-- this is only an example-->
+
+            <p class="game-description">{{ game.gameDesc }}</p>
+
+            <div class="avaible-container">
+                <h3>Avaible on:</h3>
+                <ul>
+                    <li v-if="game.gameSteamLink">
+                        <a :href="game.gameSteamLink" target="_blank">
+                            <img src='../assets/avaibleIcons/SteamLogo.svg' alt="Steam Logo" />
+                        </a>
+                    </li>
+                    <li v-if="game.gameGogLink">
+                        <a :href="game.gameGogLink" target="_blank">
+                            <img src='../assets/avaibleIcons/GogLogo.svg' alt="Gog Logo" />
+                        </a>
+                    </li>
+                    <li v-if="game.gameEpicLink">
+                        <a :href="game.gameEpicLink" target="_blank">
+                            <img src='../assets/avaibleIcons/EpicGamesLogo.svg' alt="Epic Games Logo" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </main>
+
+    </div>
+    <div v-else>
+        <p>Loading...</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/style-variables.scss" as style-variables;
+
+.game-detail-page {
+    margin: 0;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: style-variables.$default-background-color;
+    overflow-y: auto;
+}
+
+.game-title {
+    font-size: 3rem;
+    color: style-variables.$default-text-color;
+}
+
+.game-description {
+    font-size: 1.5rem;
+    color: style-variables.$default-text-color;
+}
+
+.content-wrapper {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 20px;
+    padding-bottom: 3%;
+}
+</style>
