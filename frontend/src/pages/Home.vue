18ab4b9ff0a534db96518448eacd4c9fd0c1d7e5
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-15 18:09:47 +0100

    feat: added the mysql logic and implemented in the home

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const categories = ref([]);
+
+const fetchCategories = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/api/categories');
+        const data = await response.json();
+        categories.value = data;
+
+        await Promise.all(categories.value.map(async (category) => {
+            try {
+                const gamesResponse = await fetch(`http://localhost:3000/api/games?${category.name}`);
+                const gamesData = await gamesResponse.json();
+                category.games = gamesData;
+                category.games = category.games.slice(0, 5);
+            }
+            catch (error) {
+                console.error(`Error fetching games for category ${category.name}:`, error);
+                category.games = []; //fallback
+            }
+
+
+        }));
+    }
+    catch (error) {
+        console.error('Error fetching categories:', error);
+    }
+};
+
+onMounted(() => {
+    fetchCategories();
+});
+</script>
+
+
 <template>
     <div class="homepage">
         <main class="content-wrapper">
@@ -8,41 +45,25 @@
                 </button>
 
             </div>
-            <h2 class="section-title">Action</h2>
-            <div class="game-section">
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-            </div>
-            <h2 class="section-title">Shooter</h2>
-            <div class="game-section">
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
-                <div class="game-card">
-                    <!-- Here the list of the games -->
-                </div>
+
+            <div v-for="category in categories" :key="category.id">
+                <template v-if="category.games && category.games.length > 0">
+                    <h2 class="section-title">{{ category.name }}</h2>
+                    <div class="game-section">
+                        <router-link
+                            v-for="game in category.games"
+                            :key="game.id"
+                            :to="`/game/${game.id}`"
+                            class="game-card"
+                        >
+                            <img
+                                :src="`data:image/jpg;base64,${btoa(String.fromCharCode(...new Uint8Array(game.gameCoverBin.data)))}`"
+                                :alt="game.gameName"
+                                class="game-cover-image"
+                            />
+                        </router-link>
+                    </div>
+                </template>
             </div>
         </main>
 
@@ -55,7 +76,7 @@
 </script>
 
 <style scoped lang="scss">
-    @use "../styles/style-variables.scss" as style-variables;
+@use "../styles/style-variables.scss" as style-variables;
 
 .homepage {
     margin: 0;
@@ -73,7 +94,7 @@
     padding-bottom: 3%;
 }
 
-.search-container{
+.search-container {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -82,7 +103,7 @@
     margin: 40px auto 20px auto;
     background-color: style-variables.$default-navbar-color;
 
-    .search-input{
+    .search-input {
         flex: 1;
         border: none;
         background-color: transparent;
@@ -90,7 +111,8 @@
         outline: none;
         padding: 10px 15px;
     }
-    .search-button{
+
+    .search-button {
         padding: 0px 15px;
         border: none;
         background-color: transparent;
@@ -116,6 +138,9 @@
 }
 
 .game-card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 19.2%;
     height: auto;
     aspect-ratio: 2/3;
@@ -125,6 +150,12 @@
     transition: transform 0.3s;
     min-width: 228px;
     min-height: 342px;
+
+    .game-cover-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 }
 
 .game-card:hover {
