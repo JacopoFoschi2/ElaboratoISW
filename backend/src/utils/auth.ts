e88407d6c602f18da08aafd38fc7ad77650012d5
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-16 14:29:55 +0100

    refactor: moved userHandling in auth to be consistent with util role and removed queryHandling util

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express"
-import jwt from "jsonwebtoken"
-import { User } from "../types/user"
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../types/user";
 
 // Utilizzare una variabile d'ambiente per il secret in progetti reali!
 // E anche un secret più complesso.
-const JWT_SECRET = "foo"
-const COOKIE_NAME = "site-access-token"
+const JWT_SECRET = "foo";
+const COOKIE_NAME = "site-access-token";
 
 /**
  * Codifies the user in an access token and sets it as a cookie.
@@ -13,15 +13,15 @@ const COOKIE_NAME = "site-access-token"
  */
 export const setUser = (req: Request, res: Response, user: any) => {
   // Create the access token with JWT
-  const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1 day" })
+  const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1 day" });
   // Set the access token as a cookie
   res.cookie(COOKIE_NAME, accessToken, {
     maxAge: 86400000, // 1 day in milliseconds
     httpOnly: true,
     sameSite: true,
     // secure: true
-  })
-}
+  });
+};
 
 /**
  * Decodes and verifies the access token, returning the user.
@@ -29,16 +29,16 @@ export const setUser = (req: Request, res: Response, user: any) => {
  */
 export const getUser = (req: Request, res: Response) => {
   // Get the access token cookie
-  const accessToken = req.cookies[COOKIE_NAME]
+  const accessToken = req.cookies[COOKIE_NAME];
   // Return the user data contained in the access token, or null if the token is missing or invalid
-  if (!accessToken) return null
+  if (!accessToken) return null;
   try {
-    const user = jwt.verify(accessToken, JWT_SECRET) as User
-    return user
+    const user = jwt.verify(accessToken, JWT_SECRET) as User;
+    return user;
   } catch {
-    return null
+    return null;
   }
-}
+};
 
 /**
  * Deletes the cookie containing the access token.
@@ -46,5 +46,56 @@ export const getUser = (req: Request, res: Response) => {
  */
 export const unsetUser = (req: Request, res: Response) => {
   // Delete the access token cookie
-  res.clearCookie(COOKIE_NAME)
-}
+  res.clearCookie(COOKIE_NAME);
+};
+
+/**
+ * Handles existence check queries and sends appropriate HTTP responses.
+ *
+ * @param res - The Express Response object used to send the HTTP response.
+ * @param f - An asynchronous function that returns a Promise resolving to a boolean indicating existence.
+ */
+export const handleExists = async (
+  res: Response,
+  f: () => Promise<boolean>
+) => {
+  try {
+    const isTaken = await f();
+    res.status(200).json({ exists: isTaken });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/**
+ * Handles user authentication and role verification.
+ *
+ * @param req - The Express Request object containing the user information.
+ * @param res - The Express Response object used to send the HTTP response.
+ * @param requiredRoles - An array of roles that are permitted to access the resource.
+ * @returns A Promise that resolves to the authenticated User object if successful, or undefined if authentication or authorization fails.
+ */
+export const handleUser = async (
+  req: Request,
+  res: Response,
+  requiredRoles: string[]
+): Promise<User | undefined> => {
+  const user = getUser(req, res);
+  if (!user) {
+    res.sendStatus(401);
+    return undefined;
+  }
+
+  if (!handleRole(res, user.userRole, requiredRoles)) {
+    return undefined;
+  }
+  return user;
+};
+
+const handleRole = (res: Response, role: string, requiredRoles: string[]) => {
+  if (!requiredRoles.includes(role)) {
+    res.status(403).send("Forbidden");
+    return false;
+  }
+  return true;
+};
