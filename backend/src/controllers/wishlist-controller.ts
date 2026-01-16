55e6019beda89d3acdedf3154780aba8474e6cd0
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-16 15:25:14 +0100

    refactor: fixed indentation with prettier

--- backend/src/controllers/wishlist-controller.ts
+++ backend/src/controllers/wishlist-controller.ts
@@ -1,60 +1,65 @@
-import type { Request, Response } from "express"
-import { connection } from "../utils/db-connection"
+import type { Request, Response } from "express";
+import { connection } from "../utils/db-connection";
 import { handleUser } from "../utils/auth";
-import { handleExists } from "../utils/query-handling"
+import { handleExists } from "../utils/query-handling";
 
 export const addToWishlist = async (req: Request, res: Response) => {
-    const user = await handleUser(req, res, ["user", "admin", "master"]);
-    if (!user) {
-        return;
-    }
-    
-    await connection.execute(
-        `INSERT INTO wishlist (userId, gameId) VALUES (?, ?)`,
-        [user.userId, req.params["gameId"]]
-    )
-    res.status(201).send("Game added to wishlist");
+  const user = await handleUser(req, res, ["user", "admin", "master"]);
+  if (!user) {
+    return;
+  }
+
+  await connection.execute(
+    `INSERT INTO wishlist (userId, gameId) VALUES (?, ?)`,
+    [user.userId, req.params["gameId"]]
+  );
+  res.status(201).send("Game added to wishlist");
 };
 
 export const listWishlistOfUser = async (req: Request, res: Response) => {
-    const user = await handleUser(req, res, ["user", "admin", "master"]);
-    if (!user) {
-        return;
-    }
+  const user = await handleUser(req, res, ["user", "admin", "master"]);
+  if (!user) {
+    return;
+  }
 
-    const [wishlist] = await connection.execute(
-        `SELECT gameId FROM wishlist WHERE userId = ?`,
-        [user.userId]
-    )
-    res.status(200).json(wishlist);
+  const [wishlist] = await connection.execute(
+    `SELECT gameId FROM wishlist WHERE userId = ?`,
+    [user.userId]
+  );
+  res.status(200).json(wishlist);
 };
 
 export const deleteFromWishlist = async (req: Request, res: Response) => {
-    const user = await handleUser(req, res, ["user", "admin", "master"]);
-    if (!user) {
-        return;
-    }
+  const user = await handleUser(req, res, ["user", "admin", "master"]);
+  if (!user) {
+    return;
+  }
 
-    await connection.execute(
-        `DELETE FROM wishlist WHERE userId = ? AND gameId = ?`,
-        [user.userId, req.params["gameId"]]
-    )
-    res.status(200).send("Game removed from wishlist");
+  await connection.execute(
+    `DELETE FROM wishlist WHERE userId = ? AND gameId = ?`,
+    [user.userId, req.params["gameId"]]
+  );
+  res.status(200).send("Game removed from wishlist");
 };
 
 export const isGameInWishlist = async (req: Request, res: Response) => {
-    const user = await handleUser(req, res, ["user", "admin", "master"]);
-    if (!user) {
-        return;
-    }
+  const user = await handleUser(req, res, ["user", "admin", "master"]);
+  if (!user) {
+    return;
+  }
 
-    handleExists(res, () => checkWishlist(user.userId, Number(req.params["gameId"])));
+  handleExists(res, () =>
+    checkWishlist(user.userId, Number(req.params["gameId"]))
+  );
 };
 
-const checkWishlist = async (userId: number, gameId: number): Promise<boolean> => {
-    const [rows] = await connection.execute(
-        `SELECT * FROM wishlist WHERE userId = ? AND gameId = ?`,
-        [userId, gameId]
-    );
-    return (Array.isArray(rows) && rows.length > 0);
-}
+const checkWishlist = async (
+  userId: number,
+  gameId: number
+): Promise<boolean> => {
+  const [rows] = await connection.execute(
+    `SELECT * FROM wishlist WHERE userId = ? AND gameId = ?`,
+    [userId, gameId]
+  );
+  return Array.isArray(rows) && rows.length > 0;
+};
