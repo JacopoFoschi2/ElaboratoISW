b665c91b6c3dc98e1d4e885252acdcd3614eb762
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-16 14:22:16 +0100

    feat: removed now useless output handling and added utils for user existence and authorization

--- backend/src/utils/query-handling.ts
+++ backend/src/utils/query-handling.ts
@@ -1,14 +1,6 @@
-import { Response } from "express";
-
-export function handleQueryOutput(code: number, res: Response) {
-    return function (err: any, results: any, fields: any) {
-    if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-    }
-    res.status(code).json(results);
-    };
-}
+import { Request, Response } from "express";
+import { User } from "../types/user";
+import { getUser } from "./auth";
 
 /**
  * Handles existence check queries and sends appropriate HTTP responses.
@@ -24,3 +16,32 @@ export const handleExists = async (res: Response, f: () => Promise<boolean>) => 
       res.status(500).json({ error: err.message });
     }
 }
+
+/**
+ * Handles user authentication and role verification.
+ *
+ * @param req - The Express Request object containing the user information.
+ * @param res - The Express Response object used to send the HTTP response.
+ * @param requiredRoles - An array of roles that are permitted to access the resource.
+ * @returns A Promise that resolves to the authenticated User object if successful, or undefined if authentication or authorization fails.
+ */
+export const handleUser = async (req: Request, res: Response, requiredRoles: string[]): Promise<User | undefined> => {
+  const user = getUser(req, res);
+      if (!user) {
+          res.sendStatus(401);
+          return undefined;
+      }
+      
+      if(!handleRole(res, user.userRole, requiredRoles)) {
+          return undefined;
+      }
+      return user;
+}
+
+const handleRole = (res: Response, role: string, requiredRoles: string[]) => {
+     if (!requiredRoles.includes(role)) {
+         res.status(403).send("Forbidden");
+         return false;
+     }
+     return true;
+ }
