bafe2009ffb6f16e5cd4dabea969e03782966693
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-16 12:00:17 +0100

    feat: adapted authentication router to support all routes

--- backend/src/routes/authentication-router.ts
+++ backend/src/routes/authentication-router.ts
@@ -3,7 +3,9 @@ import * as authController from "../controllers/authentication-controller"
 
 const router = Router()
 
-router.post("/api/auth/register", authController.register)
+router.post("/api/auth/register", authController.createUser)
+router.get("/api/auth/username-exists/:username", authController.isUsernameTaken)
+router.get("/api/auth/email-exists/:email", authController.isEmailRegistered)
 router.post("/api/auth/login", authController.login)
 router.post("/api/auth/logout", authController.logout)
 router.get("/api/auth/profile", authController.getProfile)
