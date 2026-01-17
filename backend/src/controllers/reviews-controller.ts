f86a17f87c628ebe756591214ea22946ef9898e1
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-17 10:39:55 +0100

    refactor: removed duplicated query and generalized listReviewsOfGame to put review of user on top

--- backend/src/controllers/reviews-controller.ts
+++ backend/src/controllers/reviews-controller.ts
@@ -21,23 +21,29 @@ export const createReview = async (req: Request, res: Response) => {
 };
 
 export const listReviewsOfGame = async (req: Request, res: Response) => {
-  const [reviews] = await connection.execute(
-    `SELECT * FROM reviews WHERE gameId = ? ORDER BY reviewTimeStamp DESC`,
-    [req.params["gameId"]]
-  );
-  res.status(200).json(reviews);
-};
-
-export const listReviewsOfUser = async (req: Request, res: Response) => {
   const user = await handleUser(req, res, ["user", "admin", "master"]);
-  if (!user) {
-    return;
+
+  let query = `
+    SELECT *
+    FROM reviews
+    WHERE gameId = ?
+  `;
+
+  const params: any[] = [req.params["gameId"]];
+
+  if (user) {
+    query += `
+      ORDER BY (userId = ?) DESC, reviewTimeStamp DESC
+    `;
+    params.push(user.userId);
+  } else {
+    query += `
+      ORDER BY reviewTimeStamp DESC
+    `;
   }
 
-  const [reviews] = await connection.execute(
-    `SELECT * FROM reviews WHERE userId = ? ORDER BY reviewTimeStamp DESC`,
-    [user.userId]
-  );
+  const [reviews] = await connection.execute(query, params);
+
   res.status(200).json(reviews);
 };
 
